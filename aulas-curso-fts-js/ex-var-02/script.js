50d69ff2dbf1b6a232bf4315b1dae3dabee0101a
RomilsonCordeiro <enderpigonze@gmail.com>
2025-09-24 14:57:44 -0300

    add aula length e var

---
+++ aulas-curso-fts-js/ex-var-02/script.js
@@ -0,0 +1,4 @@
+let n1 = "Jesus";
+let n2 = "Cristo";
+
+console.log(n1 + " " + n2);
---
+++ aulas-curso-fts-js/init-aula-length/script.js
@@ -0,0 +1,9 @@
+function comprimento() {
+  let nome = "Romilson"; /* Variavel do nome */
+  let comp = nome.length; /* variavel do comprimento */
+
+  console.log(
+    nome + " tem " + comp + " caracteres." + " Ainda restam " + (140 - comp)
+  ); /* Mostrando o resultado na tela */
+}
+comprimento(); /* Chamando a função */
